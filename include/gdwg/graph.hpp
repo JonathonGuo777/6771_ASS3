1caf1d29fa718f3583875314a00f3eb294f52937
z5335534 Ao Guo <z5335534@ad.unsw.edu.au>
2022-08-06 13:40:00 +0800

    4

--- include/gdwg/graph.hpp
+++ include/gdwg/graph.hpp
@@ -165,35 +165,35 @@ namespace gdwg {
 		auto merge_replace_node(N const& old_data, N const& new_data) -> void {
 			auto old_iter = nodes_.find(old_data);
 			auto new_iter = nodes_.find(new_data);
-			if (old_iter == nodes_.end() or new_iter == nodes_.end()) {
-				throw std::runtime_error("Cannot call gdwg::graph<N, E>::merge_replace_node on old or "
-				                         "new data if they don't exist in the graph");
-			}
 
-			// Find all relevant nodes
-			auto edge_ptrs = std::vector<std::shared_ptr<edge>>();
-			std::copy_if(edges_.begin(),
-			             edges_.end(),
-			             std::back_inserter(edge_ptrs),
-			             [&](auto const& e_ptr) {
-				             return *(e_ptr->src) == old_data or *(e_ptr->dst) == old_data;
-			             });
+			if (old_iter != nodes_.end() and  new_iter != nodes_.end()) {
+				// Find all relevant nodes
+				auto edge_ptrs = std::vector<std::shared_ptr<edge>>();
+				std::copy_if(edges_.begin(),
+				             edges_.end(),
+				             std::back_inserter(edge_ptrs),
+				             [&](auto const& e_ptr) {
+					             return *(e_ptr->src) == old_data or *(e_ptr->dst) == old_data;
+				             });
 
-			// Merge the nodes
-			for (auto const& e_ptr : edge_ptrs) {
-				auto new_src_ptr = *(e_ptr->src) == old_data ? (*new_iter).get() : e_ptr->src;
-				auto new_dst_ptr = *(e_ptr->dst) == old_data ? (*new_iter).get() : e_ptr->dst;
+				// Merge the nodes
+				for (auto const& e_ptr : edge_ptrs) {
+					auto new_src_ptr = *(e_ptr->src) == old_data ? (*new_iter).get() : e_ptr->src;
+					auto new_dst_ptr = *(e_ptr->dst) == old_data ? (*new_iter).get() : e_ptr->dst;
 
-				struct edge new_edge = edge{new_src_ptr, new_dst_ptr, e_ptr->weight};
-				edges_.erase(e_ptr);
+					struct edge new_edge = edge{new_src_ptr, new_dst_ptr, e_ptr->weight};
+					edges_.erase(e_ptr);
 
-				// If the new edge not exist, insert it
-				if (edges_.find(new_edge) == edges_.end()) {
-					edges_.emplace(std::make_shared<edge>(new_edge));
+					// If the new edge not exist, insert it
+					if (edges_.find(new_edge) == edges_.end()) {
+						edges_.emplace(std::make_shared<edge>(new_edge));
+					}
 				}
+				// Remove the old node
+				nodes_.erase(old_iter);
 			}
-			// Remove the old node
-			nodes_.erase(old_iter);
+			throw std::runtime_error("Cannot call gdwg::graph<N, E>::merge_replace_node on old or "
+			                         "new data if they don't exist in the graph");
 		}
 
 		auto erase_node(N const& value) -> bool {
